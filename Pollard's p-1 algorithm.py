f8c204eb7ab6aa8f005c15337dd03e0b53f18622
jhamer32993e <60753704+jhamer32993e@users.noreply.github.com>
2024-04-09 13:20:30 +0100

    Update Pollard's p-1 algorithm.py

--- Pollard's p-1 algorithm.py
+++ Pollard's p-1 algorithm.py
@@ -10,10 +10,10 @@ def euclid(a1, b):
         mya = myb
         myb = r
     return mya
-
+# Euclid's algorithm function to avoid import math then math.gcd
 
 n = open('PrimeList.csv', 'r')
-# File of primes up to 10^8
+# File of primes up to 10^7
 Lines = n.readlines()
 # Choose 2 random primes for factorisation
 p1 = int(Lines[random.randint(1, len(Lines))])
